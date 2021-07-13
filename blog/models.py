1a977497c1febd144f94d6e567ad1d62fc8e427a
hiagoleresdev <hiagoleresdev@gmail.com>
2021-07-12 22:47:53 -0300

    Projeto finalizado

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.db.models.lookups import IntegerFieldFloatRounding
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added'] 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added'] 
